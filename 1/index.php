f06e91ac24d5e35e13e6c831147df1d4c54f7263
SlavikTZ <Slavik.tz@yandex.ru>
2016-08-23 15:17:26 +0300

    На работе

--- 1/index.php
+++ 1/index.php
@@ -1,1 +1,17 @@
-<?php
+<?php include 'table.php' ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document1</title>
+    </head>
+    <body>
+        <table border=1 cellspacing="0">
+            <?php 
+                $table = new Arr(10); 
+                $table->viewTable();
+            ?>
+        </table>
+
+    </body>
+</html>
